ef9098823dcc635c2a157fd83dd2e0f23ad55b3f
FuryDev <105069460+chithilamanul1@users.noreply.github.com>
2025-10-05 14:30:10 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'images.unsplash.com',
-            },
-            {
-                protocol: 'https',
-                hostname: 'placehold.co',
-            },
-        ],
-    },
+  output: 'export', // required for next export on Netlify
+  reactStrictMode: true, // optional but recommended
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
