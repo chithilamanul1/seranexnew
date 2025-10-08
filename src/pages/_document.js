2d3f6bd04d676d332fe4c7fff3b446e6f53ddb19
FuryDev <105069460+chithilamanul1@users.noreply.github.com>
2025-10-08 19:06:00 +0530

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,10 +4,9 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=optional"
-          rel="stylesheet"
-        />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700;800&display=optional" rel="stylesheet" />
       </Head>
       <body>
         <Main />
