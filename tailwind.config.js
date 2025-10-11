ba1f2f5ec1d5323664dda509d390218408723862
FuryDev <105069460+chithilamanul1@users.noreply.github.com>
2025-10-11 19:30:44 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,9 +10,9 @@ module.exports = {
     extend: {
       colors: {
         primary: {
-          DEFAULT: '#2563eb', // A professional blue (blue-600)
-          'light': '#3b82f6', // blue-500
-          'dark': '#1d4ed8',  // blue-700
+          DEFAULT: '#2563eb',
+          'light': '#3b82f6',
+          'dark': '#1d4ed8',
         },
       },
       animation: {
