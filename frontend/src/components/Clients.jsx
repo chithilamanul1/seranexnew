1b970efa78defec8bd062a22a7b2411f92a7579e
chithilamanul1 <chithilamanul@gmail.com>
2026-07-17 11:03:47 +0530

    Remove color inversion from client logos

--- frontend/src/components/Clients.jsx
+++ frontend/src/components/Clients.jsx
@@ -12,7 +12,7 @@ const Clients = () => (
           <img
             src={client.logo}
             alt={client.name}
-            className="sm:w-[192px] w-[100px] object-contain rounded-lg bg-gray-100 dark:bg-white/5 p-3 hover:bg-gray-200 dark:hover:bg-white/10 transition-all duration-300 filter dark:invert-0 invert"
+            className="sm:w-[192px] w-[100px] object-contain rounded-lg bg-gray-100 dark:bg-white/5 p-3 hover:bg-gray-200 dark:hover:bg-white/10 transition-all duration-300"
           />
         </div>
       ))}
